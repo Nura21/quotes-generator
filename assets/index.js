db170c869f9f5d43eabc63ce1a3d82b60148a920
Nura21 <m.irfansyafalah@gmail.com>
2022-05-12 00:31:31 +0700

    update JSON

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,28 @@
 function generate(){
     //CREATE ARRAY DATA
-    var fruits = ["Banana", "Orange", "Apple", "Mango"];
+    var quotes = '{"quotes1" : "agusph", "quotes2" : "Agus Prawoto Hadi", "quotes3" : "Jagowebdev.com"}';
+    //MAKE JSON
+    quotesObj = JSON.parse(quotes);
 
     //CREATE RANDOM CALL
     var randomNumber = Math.floor(Math.random() * 4);
+    
+    //CREATE DATA
+    var quotesData = '';
+        if(randomNumber == 1){
+            quotesData = quotesObj.quotes1;
+        }else if(randomNumber == 2){
+            quotesData = quotesObj.quotes2;
+        }else{
+            quotesData = quotesObj.quotes3;
+        }
 
     //GET id words
     let words = document.getElementById("words");
-    words.innerHTML = fruits[randomNumber];
+    words.innerHTML = quotesData;
+
+
+
+
 }
+
